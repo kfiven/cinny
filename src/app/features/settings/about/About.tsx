38f8046277b0fb6d66e10956b7a2879074e8dd75
kfiven <33421343+kfiven@users.noreply.github.com>
2026-03-19 05:52:32 +0000

    chore(release): 4.10.12 [skip ci]

--- src/app/features/settings/about/About.tsx
+++ src/app/features/settings/about/About.tsx
@@ -46,7 +46,7 @@ export function About({ requestClose }: AboutProps) {
                   <Box direction="Column" gap="100">
                     <Box gap="100" alignItems="End">
                       <Text size="H3">Cinny</Text>
-                      <Text size="T200">v4.10.11</Text>
+                      <Text size="T200">v4.10.12</Text>
                     </Box>
                     <Text>Yet another matrix client.</Text>
                   </Box>
